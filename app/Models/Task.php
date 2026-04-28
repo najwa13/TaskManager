2232b6535296a5ff7cb9f74c793de05a17164529
najwa13 <najwafathi05@hotmail.com>
2026-04-28 13:02:26 +0100

    fix: add HasFactory trait to Category and Task models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
+    use HasFactory;
     protected $fillable = [ 'title','description','status','due_date','user_id','category_id'];
     public function user(): BelongsTo
     {
