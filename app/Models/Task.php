73bfe8e5fc80082dd5cf207ea36e709634166810
najwa13 <najwafathi05@hotmail.com>
2026-04-28 10:35:23 +0100

    feat: create Category and Task models

---
+++ app/Models/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    protected $fillable = [ 'title','description','status','due_date','user_id','category_id'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
