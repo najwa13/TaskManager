ecdf8d17958385721f4d2fd2d70d5c9a7a9d02e3
najwa13 <najwafathi05@hotmail.com>
2026-04-29 16:31:44 +0100

    routing: add task resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('tasks', TaskController::class);
 });
 
 require __DIR__.'/auth.php';
